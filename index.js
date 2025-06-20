9a46b84feae4862a14cda0ca05b307995164c032
jyhugo <165433596+jyhugo@users.noreply.github.com>
2025-06-20 11:00:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ function criarBot() {
         host: 'CraftIFMA.aternos.me',
         port: 21968,
         username: 'Hedrinho',
-        offline: false,           // se for login com Microsoft
-        authTitle: 'minecraft',   // necessário para contas Microsoft
-        skipPing: true
+        skipPing: true,
+        auth: {
+            type: 'microsoft',
+            flow: 'msal' // <<< ISSO AQUI É ESSENCIAL
+        }
     });
 
     client.on('join', () => {
@@ -17,7 +19,7 @@ function criarBot() {
 
     client.on('disconnect', (reason) => {
         console.log('❌ Desconectado:', reason);
-        setTimeout(criarBot, 10000); // tenta reconectar após 10 segundos
+        setTimeout(criarBot, 10000); // reconecta depois de 10s
     });
 
     client.on('error', (err) => {
@@ -27,7 +29,7 @@ function criarBot() {
 
 criarBot();
 
-// === HTTP SERVER FALSO PARA MANTER RENDER ATIVO ===
+// HTTP SERVER FAKE PRA ENGANAR O RENDER
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
